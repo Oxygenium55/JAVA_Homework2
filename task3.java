f5572df03c504aed547f5973c27c5a9fe4db064a
Oxygen <grandnorik@mail.ru>
2023-03-12 20:15:34 +0600

    in process

--- task3.java
+++ task3.java
@@ -3,6 +3,18 @@
 
 package homework2;
 
+import java.util.Scanner;
+
 public class task3 {
-    
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String string = in.next();
+        System.out.println(isPalindrom(string));
+        in.close();
+    }
+
+    public static boolean isPalindrom(String string) {
+        return string.equals((new StringBuilder(string)).reverse().toString());
+    }
 }
